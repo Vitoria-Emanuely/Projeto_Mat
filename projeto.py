7662a9d8dde62e43a3c49df06ea6edb07d23bdfd
Vitória Emanuely <vikaemanuely@gmail.com>
2020-04-15 19:34:18 -0300

    Improve test

--- projeto.py
+++ projeto.py
@@ -1,10 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 #Início
-a = 0
+a = 0.0
 
 #Fim
-b = 1
+b = 1.0
 
 #Números de pontos
 n = 5
@@ -19,19 +20,23 @@ A = np.zeros((n,n))
 B = np.zeros(n)
 
 #Valores fixos
-A[0, 0] = 2
-A[0, 1] = -1
-A[n - 1, n - 1] = 2
-A[n - 1, n - 2] = -1
+A[0, 0] = 2.0
+A[0, 1] = -1.0
+A[n - 1, n - 1] = 2.0
+A[n - 1, n - 2] = -1.0
 
 #Preenche as matrizes A e B
 for i in np.arange(1, n - 1):
-    A[i, i] = 2
-    A[i, i + 1] = -1
-    A[i, i - 1] = -1
+    A[i, i] = 2.0
+    A[i, i + 1] = -1.0
+    A[i, i - 1] = -1.0
 
     B[i] = 100 * deltax**2 * (x[i] - 1)**2
 
 #Resolve a matriz linear
 U = np.linalg.solve(A,B)
 print(U)
+
+#Gera o gráfico (arredonda as casas decimais)
+plt.plot(x, U)
+plt.show()
